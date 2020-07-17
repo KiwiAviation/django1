06b6a19eaa6e7d4a3553b672f2ec46b0a7b35396
KiwiAviation <jacobkeller737@gmail.com>
2020-07-16 17:18:02 -0700

    Created Tent_db interfacing class

--- qm/spreadsheet.py
+++ qm/spreadsheet.py
@@ -57,3 +57,60 @@ class mSheet():
         mSheet.sheet.delete_rows(cell.row)
         mSheet.sheet.insert_row(new, cell.row)
         return
+
+class tSheet():
+    '''class used to access and edit the Tent_db Google Sheet'''
+    # vars for access
+    scope = ['https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    # sheet to access
+    sheet = client.open('Tent_db').sheet1
+ 
+    # search methods
+    def get_cell(row, col):
+        '''returns the value of a cell with the given position'''
+        return tSheet.sheet.cell(row, col).value
+ 
+    def get_row(row):
+        '''returns a list with all values in the given row'''
+        return tSheet.sheet.row_values(row)
+ 
+    def get_all():
+        '''returns list of lists with all values in the spreadsheet'''
+        return tSheet.sheet.get_all_values()
+ 
+    def find_cell(value):
+        '''returns all cells with the given search value'''
+        return tSheet.sheet.findall(value)
+ 
+    def find_row(value):
+        '''finds the row with the search value and returns a list of all values within the row'''
+        cell = tSheet.sheet.find(value)
+        return tSheet.sheet.row_values(cell.row)
+ 
+    # edit methods
+    def update_cell(row, col, new):
+        '''updates a cell at the given position'''
+        tSheet.sheet.update_cell(row, col, new)
+        return
+ 
+    def insert_row(new, row):
+        '''inserts a new row at the position given'''
+        tSheet.sheet.insert_row(new, row)
+        return
+ 
+    def update_cell_by_val(value, new):
+        '''finds all cells with the given search value and updates each cell with the new value'''
+        cells = tSheet.sheet.findall(value)
+        for cell in cells:
+            tSheet.sheet.update_cell(cell.row, cell.col, new)
+        return
+ 
+    def update_row_by_val(value, new):
+        '''finds the row with the given search value and inserts a new row at the position given'''
+        cell = tSheet.sheet.find(value)
+        tSheet.sheet.delete_rows(cell.row)
+        tSheet.sheet.insert_row(new, cell.row)
+        return
