f91d888b019e5f6deff53a30c770c9bda6665e1b
KiwiAviation <jacobkeller737@gmail.com>
2020-07-18 11:43:31 -0700

    Reformated spreadsheet.py Sheet class

--- qm/spreadsheet.py
+++ qm/spreadsheet.py
@@ -1,138 +1,85 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-class mSheet():
-    '''class used to access and edit the Master_qm Google Sheet'''
-    # vars for access
-    scope = ['https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    client = gspread.authorize(creds)
-    # sheet to access
-    sheet = client.open('Master_qm').sheet1
-
-    # search methods
-    def get_cell(row, col):
-        '''returns the value of a cell with the given position'''
-        return mSheet.sheet.cell(row, col).value
-
-    def get_row(row):
-        '''returns a list with all values in the given row'''
-        return mSheet.sheet.row_values(row)
-
-    def get_all():
-        '''returns list of lists with all values in the spreadsheet'''
-        return mSheet.sheet.get_all_values()
-
-    def find_cell(value):
-        '''returns all cells with the given search value'''
-        return mSheet.sheet.findall(value)
-
-    def find_row(value):
-        '''finds the row with the search value and returns a list of all values within the row'''
-        cell = mSheet.sheet.find(value)
-        return mSheet.sheet.row_values(cell.row)
-
-    # edit methods
-    def update_cell(row, col, new):
-        '''updates a cell at the given position'''
-        mSheet.sheet.update_cell(row, col, new)
-        return
-
-    def insert_row(new, row):
-        '''inserts a new row at the position given'''
-        mSheet.sheet.insert_row(new, row)
-        return
-
-    def update_cell_by_val(value, new):
-        '''finds all cells with the given search value and updates each cell with the new value'''
-        cells = mSheet.sheet.findall(value)
-        for cell in cells:
-            mSheet.sheet.update_cell(cell.row, cell.col, new)
-        return
-
-    def update_row_by_val(value, new):
-        '''finds the row with the given search value and inserts a new row at the position given'''
-        cell = mSheet.sheet.find(value)
-        mSheet.sheet.delete_rows(cell.row)
-        mSheet.sheet.insert_row(new, cell.row)
-        return
-
-class tSheet():
-    '''class used to access and edit the Tent_db Google Sheet'''
-    # vars for access
-    scope = ['https://spreadsheets.google.com/feeds',
-            'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-    client = gspread.authorize(creds)
-    # sheet to access
-    sheet = client.open('Tent_db').sheet1
-
+class Sheet():
+    '''class used to access and edit Google Sheets'''
+ 
+    def __init__(self, open: str):
+            # vars for access
+        scope = ['https://spreadsheets.google.com/feeds',
+                'https://www.googleapis.com/auth/drive']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        client = gspread.authorize(creds)
+        # sheet to access
+        self.sheet = client.open(open).sheet1
+ 
     # helper function that allows col inputs to be name of col and row inputs to be ids (get_cell('ta1', 'condition')) 
-    def col_index(col):
+    def col_index(self, col):
         '''converts input col name to a num'''
-        for i, val in enumerate(tSheet.sheet.row_values(1)):
+        for i, val in enumerate(self.sheet.row_values(1)):
             if val == col:
                 return i + 1
         return col
-
-    def row_index(row):
+ 
+    def row_index(self, row):
         '''converts input id to a num'''
-        for i, val in enumerate(tSheet.sheet.col_values(1)):
+        for i, val in enumerate(self.sheet.col_values(1)):
             if val == row:
                 return i + 1
         return row
-
+ 
     # search methods
-    def get_cell(row, col):
-        row = tSheet.row_index(row)
-        col = tSheet.col_index(col)
+    def get_cell(self, row, col):
+        row = self.row_index(row)
+        col = self.col_index(col)
         '''returns the value of a cell with the given position'''
-        return tSheet.sheet.cell(row, col).value
+        return self.sheet.cell(row, col).value
  
-    def get_row(row):
-        row = tSheet.row_index(row)
+    def get_row(self, row):
+        row = self.row_index(row)
         '''returns a list with all values in the given row'''
-        return tSheet.sheet.row_values(row)
+        return self.sheet.row_values(row)
  
-    def get_all():
+    def get_all(self):
         '''returns list of lists with all values in the spreadsheet'''
-        return tSheet.sheet.get_all_values()
+        return self.sheet.get_all_values()
  
-    def find_cell(value):
+    def find_cell(self, value):
         '''returns all cells with the given search value'''
-        return tSheet.sheet.findall(value)
+        return self.sheet.findall(value)
  
-    def find_row(value):
+    def find_row(self, value):
         '''finds the row with the search value and returns a list of all values within the row'''
-        cell = tSheet.sheet.find(value)
-        return tSheet.sheet.row_values(cell.row)
+        cell = self.sheet.find(value)
+        return self.sheet.row_values(cell.row)
  
     # edit methods
-    def update_cell(row, col, new):
-        row = tSheet.row_index(row)
-        col = tSheet.col_index(col)
+    def update_cell(self, row, col, new):
+        row = self.row_index(row)
+        col = self.col_index(col)
         '''updates a cell at the given position'''
-        tSheet.sheet.update_cell(row, col, new)
+        self.sheet.update_cell(row, col, new)
         return
  
-    def insert_row(new, row):
-        row = tSheet.row_index(row)
+    def insert_row(self, new, row):
+        row = self.row_index(row)
         '''inserts a new row at the position given'''
-        tSheet.sheet.insert_row(new, row)
+        self.sheet.insert_row(new, row)
         return
  
-    def update_cell_by_val(value, new):
+    def update_cell_by_val(self, value, new):
         '''finds all cells with the given search value and updates each cell with the new value'''
-        cells = tSheet.sheet.findall(value)
+        cells = self.sheet.findall(value)
         for cell in cells:
-            tSheet.sheet.update_cell(cell.row, cell.col, new)
+            self.sheet.update_cell(cell.row, cell.col, new)
         return
  
-    def update_row_by_val(value, new):
+    def update_row_by_val(self, value, new):
         '''finds the row with the given search value and inserts a new row at the position given'''
-        cell = tSheet.sheet.find(value)
-        tSheet.sheet.delete_rows(cell.row)
-        tSheet.sheet.insert_row(new, cell.row)
+        cell = self.sheet.find(value)
+        self.sheet.delete_rows(cell.row)
+        self.sheet.insert_row(new, cell.row)
         return
- 
+
+mSheet = Sheet('Master_qm')
+tSheet = Sheet('Tent_db')
